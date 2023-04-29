514c147e11f04339bd4152d671eb0254501b9cf4
Sandeep Menon <snm6477@nyu.edu>
2023-04-28 22:00:06 -0400

    decoder export

--- our_OpenSTL/openstl/models/__init__.py
+++ our_OpenSTL/openstl/models/__init__.py
@@ -9,9 +9,9 @@ from .phydnet_model import PhyDNet_Model
 from .predrnn_model import PredRNN_Model
 from .predrnnpp_model import PredRNNpp_Model
 from .predrnnv2_model import PredRNNv2_Model
-from .simvp_model import SimVP_Model
+from .simvp_model import SimVP_Model, Decoder
 
 __all__ = [
     'ConvLSTM_Model', 'CrevNet_Model', 'E3DLSTM_Model', 'MAU_Model', 'MIM_Model',
-    'PhyDNet_Model', 'PredRNN_Model', 'PredRNNpp_Model', 'PredRNNv2_Model', 'SimVP_Model'
+    'PhyDNet_Model', 'PredRNN_Model', 'PredRNNpp_Model', 'PredRNNv2_Model', 'SimVP_Model', 'Decoder'
 ]
