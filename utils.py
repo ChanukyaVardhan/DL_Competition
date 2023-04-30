ee56aad313c09eea05d6454e7238e34e485a3399
Pranav Jangir <jangir.pranav@gmail.com>
2023-04-30 00:45:11 -0400

    [WIP] area based heuristic.

--- utils.py
+++ utils.py
@@ -33,7 +33,8 @@ def get_class_name(id):
         c_id = id
 
         return f"{shapes[s_id]}_{materials[m_id]}_{colors[c_id]}"
- 
+
+
 def get_class_ids(id):
     if id == 0:
         raise Exception("Background has no class id tuple")
@@ -48,8 +49,8 @@ def get_class_ids(id):
         return s_id, m_id, c_id
 
 
-
 class_labels = {i: get_class_name(i) for i in range(49)}
+
 
 def get_unique_objects(masks):
     B, T, H, W = masks.shape
@@ -59,15 +60,17 @@ def get_unique_objects(masks):
         per_image_unique_objects = np.array([])
         for t in range(T):
             uniq = np.unique(masks[b, t])
-            per_image_unique_objects = np.union1d(per_image_unique_objects, uniq)
-        obj_classes = [get_class_ids(i) for i in per_image_unique_objects if i != 0]
+            per_image_unique_objects = np.union1d(
+                per_image_unique_objects, uniq)
+        obj_classes = [get_class_ids(i)
+                       for i in per_image_unique_objects if i != 0]
         unique_objects.append(obj_classes)
 
     return unique_objects
 
 
 def apply_background_heuristic(S, uniq):
-    random.seed(3) # our team number
+    random.seed(3)  # our team number
     CNT = 0
     for i, obj in enumerate(uniq):
         msk = S[i].clone()
@@ -87,15 +90,49 @@ def apply_background_heuristic(S, uniq):
             CNT += 1
         for k, v in obj_mapping.items():
             S[i][S[i] == k] = v
-        
-    print("Images that need fixing : ", CNT)
+
+    print("Videos that need fixing : ", CNT)
     return S
 
+
+def get_area_and_neighbours(msk, k):
+    return 200, []
+
+
 def apply_connected_components_heuristic(S, uniq):
-    print("Not yet implemented")
+    random.seed(3)  # our team number
+    bad_cnt = 0
+    area_threshold = 100  # FIX: Change this!
+    for i, obj in enumerate(uniq):  # Iterating over batch
+        msk = S[i].clone()
+        msk = msk.detach().cpu().numpy()
+        print("msk shape : ", msk.shape)
+        uniq_msk = np.unique(msk)
+        good = True
+        known_ids = [int(1 + C + 8*B + 16*A) for (A, B, C) in obj]
+        obj_mapping = {}
+        for k in uniq_msk:
+            if k == 0:
+                continue
+            if k not in known_ids:  # Unknown object
+                good = False
+                obj_area, neighbours = get_area_and_neighbours(msk, k)
+                if obj_area < area_threshold:  # Small object
+                    obj_mapping[k] = 0
+                    if len(neighbours) > 0:
+                        # FIX: Not a fix, just highlighting.
+                        obj_mapping[k] = neighbours[0]
+
+            else:
+                obj_area, neightbours = get_area_and_neighbours(msk, k)
+        if not good:
+            bad_cnt += 1
+    print("Videos that need fixing : ", bad_cnt)
     return S
 
 # Simple heuristic: We are assigning background / known random objects to the unknown objects
+
+
 def apply_heuristics(S, uniq, method):
     # S: 1000 x H x W
     # uniq: list - 1000 of numpy arrays (uniq objs over 11 frames)
@@ -103,8 +140,9 @@ def apply_heuristics(S, uniq, method):
         return apply_background_heuristic(S, uniq)
     elif method == "connected_components":
         return apply_connected_components_heuristic(S, uniq)
-    else :
+    else:
         raise Exception("Unknown heuristic method")
+
 
 if __name__ == "__main__":
     print(class_labels)
