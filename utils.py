5eef4db8dc0eba612ff535a0a5a64c78c6c613eb
Sandeep Menon <snm6477@nyu.edu>
2023-04-30 20:33:24 -0400

    get_closest_object logic

--- utils.py
+++ utils.py
@@ -117,8 +117,29 @@ def get_area_and_neighbours(msk, k, known_ids):
     return area, [n for n in neighbours if n in known_ids]
 
 # FIX : Write a better heuristic. This is just a placeholder
+
+
 def get_closest_object(k, known_ids):
-    return 0
+    new_class_data = get_class_ids(k)   # Shape, Material, Color
+    # List of Shape, Material, Color
+    known_class_data = [get_class_ids(i) for i in known_ids]
+
+    # Check if there is a known object with same shape and color but different material
+    priorities = []
+    for i, class_data in enumerate(known_class_data):
+        priority = 0
+        priority += (class_data[0] != new_class_data[0]
+                     ) * 2    # Different shape
+        priority += (class_data[1] != new_class_data[1]
+                     ) * 1    # Different material
+        priority += (class_data[2] != new_class_data[2]
+                     ) * 4    # Different color
+        priorities.append((priority, i))
+
+    priorities.sort()
+
+    return known_ids[priorities[0][1]]
+
 
 def apply_connected_components_heuristic(S, uniq):
     random.seed(3)  # our team number
@@ -138,7 +159,8 @@ def apply_connected_components_heuristic(S, uniq):
                 continue
             if k not in known_ids:  # Unknown object
                 good = False
-                obj_area, neighbours = get_area_and_neighbours(msk, k, known_ids)
+                obj_area, neighbours = get_area_and_neighbours(
+                    msk, k, known_ids)
                 if obj_area < area_threshold:  # Small object
                     obj_mapping[k] = 0
                     if len(neighbours) > 0:
@@ -152,7 +174,8 @@ def apply_connected_components_heuristic(S, uniq):
                         obj_mapping[k] = get_closest_object(k, known_ids)
 
             else:
-                obj_area, neighbours = get_area_and_neighbours(msk, k, known_ids)
+                obj_area, neighbours = get_area_and_neighbours(
+                    msk, k, known_ids)
                 if obj_area < area_threshold:  # Small object
                     # For known objects, we assign small areas background
                     obj_mapping[k] = 0
@@ -161,7 +184,7 @@ def apply_connected_components_heuristic(S, uniq):
             bad_cnt += 1
         for k, v in obj_mapping.items():
             S[i][S[i] == k] = v
-    
+
     print("Videos that need fixing : ", bad_cnt)
     return S
 
